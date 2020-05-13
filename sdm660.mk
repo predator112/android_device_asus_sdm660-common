09fd15d5e1a7dccf602ab6431645db830038f617
Pranav Kapoor <pranavkapoor001@gmail.com>
2020-05-14 01:33:16 +0530

    sdm660-common: thermal-engine.conf: Introduce And Update sensor names for 4.19.xxx Kernel
    Co-Authored-By: Sayan Acharya <SayanAcharya@users.noreply.github.com>

--- sdm660.mk
+++ sdm660.mk
@@ -429,6 +429,9 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0-service.qti
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
+
 # Trust HAL
 PRODUCT_PACKAGES += \
     vendor.lineage.trust@1.0-service
