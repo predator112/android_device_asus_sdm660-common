61a16abf05db809f91103cea440d98d879e5e2e8
Pranav Kapoor <pranavkapoor001@gmail.com>
2021-03-30 20:53:57 +0530

    sdm660-common: Add gps blobs from LA.UM.9.12.r1-10800-SMxx50.0 and build required packages
    * switch to gps.conf, flp.conf from gps/etc/
    * Add network location overlays
    
    Signed-off-by: ElectroPerf <kunmun.devroms@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -160,6 +160,24 @@ PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl \
     android.hardware.health@2.1-service
 
+# GPS
+$(call inherit-product, $(LOCAL_PATH)/gps/gps_vendor_product.mk)
+
+PRODUCT_PACKAGES += \
+    libsensorndkbridge \
+    libwifi-hal-ctrl
+
+PRODUCT_PACKAGES += \
+    gps.conf \
+    flp.conf
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/gps/apdr.conf:$(TARGET_COPY_OUT_VENDOR)/etc/apdr.conf \
+    $(LOCAL_PATH)/configs/gps/izat.conf:$(TARGET_COPY_OUT_VENDOR)/etc/izat.conf \
+    $(LOCAL_PATH)/configs/gps/lowi.conf:$(TARGET_COPY_OUT_VENDOR)/etc/lowi.conf \
+    $(LOCAL_PATH)/configs/gps/sap.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sap.conf \
+    $(LOCAL_PATH)/configs/gps/xtwifi.conf:$(TARGET_COPY_OUT_VENDOR)/etc/xtwifi.conf
+
 # HIDL
 PRODUCT_PACKAGES += \
     libhidltransport \
